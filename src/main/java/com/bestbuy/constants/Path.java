63303504958e67a31273d78083297b4987d8d6cd
nehal vyas <114698016+jentibhai@users.noreply.github.com>
2023-01-03 23:12:34 +0000

    Initial commit

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,17 @@
+package com.petstore.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for student api
+     */
+    public static final String PET = "/pet";
+
+    public static final String USER = "/user";
+
+ // public static final String PET = "/pet";
+
+
+}
